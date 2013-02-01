0260f96e198f00d5a73f584708d5609467f30277
Christian <cpalatz@gmail.com>
2013-02-01 15:48:59 +0100

    see last commit log

--- overlay.py
+++ overlay.py
@@ -1,48 +1,71 @@
-import sys
-import threading
+import threading, time
 import random
-#import copy
+# import sys
+# import copy
 
 class Overlay:
-    def __init__(self, username, ip, bootstrappingIP, q1=None, q2=None):
+    def __init__(self, username, ip, bootstrappingIP, q1, q2, q3, q4):
         print "Enter constructor of overlay"
-        # incoming queue
-        self.inQueue = q1
-        # outgoing queue
-        self.outQueue = q2
+        # incoming queue from network layer
+        self.n2o = q1
+        # outgoing queue to network layer
+        self.o2n = q2
+        # incoming queue from application layer
+        self.a2o = q3
+        # outgoing queue to application layer
+        self.o2a = q4
         # own username
         self.ownUsername = username
         # own ip
         self.ownIP = ip
-        # list of known (max. 15) peers
+        # list of known (max. 15) peers with structure: [(username, ip)]
         self.knownPeers = []
-        # list of all (~5) neighbours
-        self.neighbours = []
+        # list of all (~5) neighbors with structure: [(username, ip, currency level)]
+        self.neighbors = []
         # dictionary of received pings
+        # TODO: change to list with currency levels
         self.ping = {}
-        # bool wether the program should terminate
+        # bool whether the program should terminate
         self.__terminated = False
         # ping to bootstrapping node
-        self.putToOutQueue(("ping", random.randint(0, 9999) , 4, 0, self.ownUsername, self.ownIP, bootstrappingIP))
-        # start thread which watches the incoming queue
-        self.inQueueThread = threading.Thread(target=self.watchInQueue)
-        self.inQueueThread.start()
+        self.putToO2N(("ping", random.randint(0, 9999) , 4, 0, self.ownUsername, self.ownIP, bootstrappingIP))
+        # start thread which watches the incoming queue from network layer
+        self.n2oThread = threading.Thread(target=self.watchN2O)
+        self.n2oThread.start()
+        # start thread which checks the currency for the neighbors
+        self.currencyThread = threading.Thread(target=self.checkCurrency)
+        self.currencyThread.start()
+        # start thread which watches the incoming queue from application layer
+        self.a2oThread = threading.Thread(target=self.watchA2O)
+        self.a2oThread.start()
     
     def terminate(self):
         print "Enter terminate()"
         self.__terminated = True
     
-    def getFromInQueue(self):
-        print "Enter getFromInQueue()"
+    def getFromN2O(self):
+        print "Enter getFromN2O()"
         message = ()
-        if self.inQueue:
-            message = self.inQueue.get(True)
+        if self.n2o:
+            message = self.n2o.get(True)
         return message
        
-    def putToOutQueue(self, message):
-        print "Enter putToOutQueue()"
-        if self.outQueue:
-            self.outQueue.put(message, True)
+    def putToO2N(self, message):
+        print "Enter putToO2N()"
+        if self.o2n:
+            self.o2n.put(message, True)
+            
+    def getFromA2O(self):
+        print "Enter getFromA2O()"
+        message = ()
+        if self.a2o:
+            message = self.a2o.get(True)
+        return message
+       
+    def putToO2A(self, message):
+        print "Enter putToO2A()"
+        if self.o2a:
+            self.o2a.put(message, True)
 
     def addToKnownPeers(self, peer):
         if not peer == (self.ownUsername, self.ownIP):
@@ -51,11 +74,12 @@ class Overlay:
                 if len(self.knownPeers) > 15:
                     self.knownPeers.pop(0)
                     
-    def addToNeighbours(self, peer):
-        if not peer == (self.ownUsername, self.ownIP):
-            if self.neighbours.count(peer) == 0:
-                if len(self.neighbours) < 5:
-                    self.neighbours.append(peer)
+    def addToNeighbours(self, peer, currency):
+        if (not peer == (self.ownUsername, self.ownIP)) and self.neighbors.count(peer) == 0 and len(self.neighbors) < 6:
+            self.neighbors.append((peer[0], peer[1], currency))
+            return True
+        else:
+            return False
                     
     def refreshNeighbours(self):
         number = 5
@@ -64,8 +88,25 @@ class Overlay:
             
         sample = random.sample(self.knownPeers, number) 
         for knownPeer in sample: 
-            self.addToNeighbours(knownPeer)
+            self.addToNeighbours(knownPeer, 2)
 
+            
+    def watchN2O(self):
+        print "Enter watchN2O()"
+        while not self.__terminated:
+            message = ()
+            if not self.n2o.empty():
+                message = self.getFromN2O()
+                if message[0] == "ping":
+                    self.processping(message)
+                    # print "Reenter watchN2O() from processping"
+                elif message[0] == "pong":
+                    self.processpong(message)
+                    # print "Reenter watchN2O() from processpong"
+                elif message[0] == "refFL":
+                    self.processIncRefFL(message)
+                else:
+                    print "Unknown message type"
             
     def processping(self, message):
         # incoming ping := ("ping", pingID, TTL, Hops, senderUsername, senderIP)
@@ -73,36 +114,36 @@ class Overlay:
         
         print "Enter processping()"
         
-        msgType, id, ttl, hops, username, ip = message
+        msgType, msgID, ttl, hops, username, ip = message
         
         # add to/refresh knownPeers list
         self.addToKnownPeers((username, ip))
         
-        if (ttl > 1) and (id not in self.ping):
+        if (ttl > 1) and (msgID not in self.ping):
             # add to ping dictionary
-            self.ping[id] = ip
+            self.ping[msgID] = ip
             # if ttl is to high
             if ttl + hops > 7:
                 ttl = 7 - hops
-            # send ping to each neighbour
-            for neighbour in self.neighbours: 
-                if not neighbour[0] == username:
-                    self.putToOutQueue((msgType, id, ttl-1, hops+1, self.ownUsername, self.ownIP, neighbour[1]))
-        elif (ttl == 1) and (id not in self.ping):
+            # send ping to each neighbor
+            for neighbor in self.neighbors: 
+                if not neighbor[0] == username:
+                    self.putToO2N((msgType, msgID, ttl-1, hops+1, self.ownUsername, self.ownIP, neighbor[1]))
+        elif (ttl == 1) and (msgID not in self.ping):
             # add to ping dictionary
-            self.ping[id] = ip
+            self.ping[msgID] = ip
             # send pong to sender
-            self.putToOutQueue(("pong", id, [(self.ownUsername, self.ownIP)], self.ping[id]))
+            self.putToO2N(("pong", msgID, [(self.ownUsername, self.ownIP)], self.ping[msgID]))
         else:
             print "Invalid ttl"
             
     def processpong(self, message):
-        # incomingpong := ("pong", ID, [(Username, IP), (Username2, IP2), ...])
-        # outgoingpong := ("pong", ID, [(Username, IP), (Username2, IP2), ...], IP)
+        # incoming pong := ("pong", ID, [(Username, IP), (Username2, IP2), ...])
+        # outgoing pong := ("pong", ID, [(Username, IP), (Username2, IP2), ...], IP)
         
         print "Enter processpong()"
         
-        msgType, id, peers = message
+        msgType, msgID, peers = message
         
         # refresh own peers list
         for peer in peers: 
@@ -113,34 +154,74 @@ class Overlay:
             peers.append((self.ownUsername, self.ownIP))
         
         # send pong to sender
-        if id in self.ping:
-            self.putToOutQueue((msgType, id, peers, self.ping[id]))
+        if msgID in self.ping:
+            self.putToO2N((msgType, msgID, peers, self.ping[msgID]))
         else:
             print "Cannot forward pong because no ping with this id has arrived before"
 
-        # refresh/fill neighbour list
+        # refresh/fill neighbor list
         self.refreshNeighbours()
+
+      
+    def checkCurrency(self):
+        print "Enter checkCurrency()"
         
+        while not self.__terminated:
             
-    def watchInQueue(self):
-        print "Enter watchInQueue()"
+            time.sleep(2)
+            
+            tmp = self.neighbors
+            for neighbor in tmp:
+                if neighbor[2] > 0:
+                    self.neighbors.append((neighbor[0], neighbor[1], neighbor[2] - 1))
+                self.neighbors.remove(neighbor)
+                
+        
+    def watchA2O(self):
+        print "Enter watchA2O()"
         while not self.__terminated:
             message = ()
-            if not self.inQueue.empty():
-                message = self.getFromInQueue()
-                if message[0] == "ping":
-                    self.processping(message)
-                    # print "Reenter watchInQueue() from processping"
-                elif message[0] == "pong":
-                    self.processpong(message)
-                    # print "Reenter watchInQueue() from processpong"
+            if not self.a2o.empty():
+                message = self.getFromA2O()
+                if message[0] == "refFL":
+                    self.processOutRefFL(message)
+                elif message[0] == "reqFile":
+                    pass
+                    # self.processReqFile(message)
+                elif message[0] == "answerReg":
+                    pass
+                    # self.processAnswerReg(message)
                 else:
                     print "Unknown message type"
-            # WARNING: the thread will terminate if the queue is empty
-            # only for correct terminating while debugging
-            # else:
-                # self.__terminated = True
-                
-            
-    def start(self):
-        print "Enter start()"
+        
+        
+    def processIncRefFL(self, message):
+        print "Enter processIncRefFL()"
+        
+        msgType, fileList, senderUsername, senderIP = message
+        
+        if (senderUsername, senderIP) in self.neighbors:
+            self.putToO2A((msgType, fileList, senderUsername, False))
+        elif self.addToNeighbours((senderUsername, senderIP), 5):
+            self.putToO2A((msgType, fileList, senderUsername, True))
+        
+        
+        
+    def processOutRefFL(self, message):
+        print "Enter processOutRefFL()"
+        
+        msgType, fileList = message
+        
+        for neighbor in self.neighbors:
+            self.putToO2N((msgType, fileList, self.ownUsername, neighbor[1]))
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- peer.py
+++ peer.py
@@ -1,35 +1,41 @@
-import sys
 from Queue import Queue
-import threading
-#import copy
+# import sys
+# import threading
+# import copy
 
 from overlay import Overlay
 #import network
 
 
-inQueue = Queue()
-outQueue = Queue()
+n2o = Queue()
+o2n = Queue()
+a2o = Queue()
+o2a = Queue()
 
-inQueue.put(("ping", 1, 4, 0, "User1", 8.1), True)
+n2o.put(("ping", 1, 4, 0, "User1", 8.1), True)
 
 # initialize and start overlay layer
-overlay = Overlay("User0", 8.0, 1.337, inQueue, outQueue)
+overlay = Overlay("User0", 8.0, 1.337, n2o, o2n, a2o, o2a)
 # overlay.start()
 
-inQueue.put(("ping", 2, 2, 2, "User2", 8.2), True)
-inQueue.put(("pong", 1, [("User1", 8.1), ("User2", 8.2), ("User3", 8.3), ("User5", 8.5), ("User6", 8.6), ("User7", 8.7)]), True)
-inQueue.put(("ping", 3, 1, 3, "User3", 8.3), True)
-inQueue.put(("ping", 4, 4, 0, "User4", 8.4), True)
+# n2o.put(("ping", 2, 2, 2, "User2", 8.2), True)
+n2o.put(("pong", 1, [("User1", 8.1), ("User2", 8.2), ("User3", 8.3), ("User5", 8.5), ("User6", 8.6), ("User7", 8.7)]), True)
+n2o.put(("ping", 3, 1, 3, "User3", 8.3), True)
+n2o.put(("ping", 4, 4, 0, "User4", 8.4), True)
 
-while not inQueue.empty():
+a2o.put(("refFL", [("File1", 800), ("File2", 600)]))
+
+while not n2o.empty():
     pass
 
 overlay.terminate()
-overlay.inQueueThread.join()
+overlay.n2oThread.join()
+overlay.currencyThread.join()
+overlay.a2oThread.join()
     
 # debug output
-# print "Number of processed messages: " + str(outQueue.qsize())
-while not outQueue.empty():
-    print outQueue.get(True)
+# print "Number of processed messages: " + str(o2n.qsize())
+while not o2n.empty():
+    print o2n.get(True)
 
 # overlay.terminate()
